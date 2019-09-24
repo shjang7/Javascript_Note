38a11d608c0db60101cd7fd28fe927be10bf1474
shjang7 <lucy.sh.jang@gmail.com>
2019-09-24 09:36:25 +0900

    Add es6_2

--- 1_2_es6.js
+++ 1_2_es6.js
@@ -244,7 +244,17 @@ class Book {
 }
 
 function makeClass() {
-
+  class Thermostat {
+    constructor(fah) {
+      this._cel = 5/9 * (fah - 32);
+    }
+    get temperature(){
+      return this._cel;
+    }
+    set temperature(updatedCel){
+      this._cel = updatedCel;
+    }
+  }
   return Thermostat;
 }
 
@@ -253,5 +263,77 @@ const thermos = new Thermostat(76);
 let temp = thermos.temperature;
 thermos.temperature = 26;
 temp = thermos.temperature;
+console.log(temp); // 26
 
-# 
+# Import and require difference
+// index.js
+import { capitalizeString } from "./string_functions"
+const cap = capitalizeString("hello!");
+
+console.log(cap);
+
+// string_functions.js
+const capitalizeString = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export { capitalizeString };
+
+export const foo = "bar";
+export const bar = "foo";
+
+# Use * to import everything from a file
+// index.js
+import * as capitalizeStrings from "capitalize_strings";
+
+# Create an export fallback with export default
+export default function subtract(x,y) {return x - y;}
+
+# Import a default export
+import subtract from "math_functions";
+
+subtract(7,4);
+
+# project
+https://www.codecademy.com/practice/projects/credit-card-checker
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
+
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
+
+
+// Add your functions below:
+const validateCred = (() => {
+  return (cred) => {
+    let newCred = [...cred].slice(cred.length - 1, 1);
+    let offset = 0;
+    for(let i = cred.length - 1; i >= 0; i--) {
+      let val = cred[i];
+      offset = (offset * 2 + val) % 9;
+    }
+    return (offset % 10 === 0) ? 'valid' : 'invalid';
+  }
+})();
+
+for(let i = 0; i < batch.length; i++) {
+	console.log(validateCred(batch[i]));
+}
